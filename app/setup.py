a0168138b56f91c4e4387bad3627ed8b277b8c2e
Ywoosang <opellong13@gmail.com>
2022-02-24 01:13:33 +0900

    Update setup

--- app/setup.py
+++ app/setup.py
@@ -1,7 +1,8 @@
-from app.config.config_db import engine
+from config.config_db import engine
 from sqlalchemy.orm import sessionmaker
-from models.models import Category
+from models.models import Category, Target
 
+# 세션 생성
 Session = sessionmaker(bind=engine)
 session = Session()
 
@@ -10,10 +11,17 @@ session.add(Category(name="휠셋"))
 session.add(Category(name="완성차"))
 session.add(Category(name="부품"))
 session.add(Category(name="구동계"))
+session.add(Category(name="용품/기타"))
 
+# 키워드 등록
+session.add(Target(keyword="보라원", category_id="1"))
+session.add(Target(keyword="비앙키", category_id="2"))
+session.add(Target(keyword="펄스락", category_id="2"))
+session.add(Target(keyword="피직 시라노", category_id="3"))
+session.add(Target(keyword="슈퍼레코드", category_id="4"))
+session.add(Target(keyword="튜블러", category_id="5"))
+session.add(Target(keyword="컨티넨탈", category_id="5"))
+
+
+# 데이터베이스에 반영
 session.commit()
-
-
-
-
-
