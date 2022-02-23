2260e9337778530d21f6440f0ac12f56477da550
Ywoosang <opellong13@gmail.com>
2022-02-24 03:59:30 +0900

    Don't make funcs to do the same

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,63 +1,53 @@
 import requests
 import json
-import os
-from typing import Dict
- 
+from typing import Dict, List
+from app.config.config_secret import secret_file_path
+
+
 class Auth:
-    client_id = "<client-id>"
-    file_path = f"{os.path.abspath(os.path.dirname(__file__))}/token.json"
+    @classmethod
+    def get_secret(cls, secret_types: List[str]) -> Dict[str,str]:
+        """json 파일로부터 요구되는 시크릿키 목록을 읽어 반환한다.
+
+        Args:
+            secret_types : 요구되는 시크릿 키 이름들을 담은 배열
+
+        Returns:
+            요구되는 시크릿 키 정보를 담은 딕셔너리
+            example:
+
+            {
+                "access_token" : "86289e71b93e7d9f67f4dcfbe69bc44d"
+                "client_id" : "86289e71b93e7d9f67f4dcfbe6123w4d"
+            }
+        """
+        with open(secret_file_path, "r") as json_file:
+            secret_info = json.load(json_file)
+            return dict(filter(
+                lambda secret:  True if secret[0] in secret_types else False, secret_info.items()))
 
     @classmethod
-    def save_token(cls,access_token: str):
+    def save_token(cls, access_token: str) -> None:
         """액세스 토큰 정보를 받아 json 파일에 저장한다.
 
         Args:
             access_token (str) : 발급한 액세스 토큰
-
-        Returns:
         """
-        with open(cls.file_path, "r") as json_file:
-            token_info = json.load(json_file)
-            token_info["access_token"] = access_token
+        with open(secret_file_path, "r") as json_file:
+            secret_info = json.load(json_file)
+            secret_info["access_token"] = access_token
 
-        with open(cls.file_path, "w") as outfile:
-            json.dump(token_info,outfile,indent=4)
+        with open(secret_file_path, "w") as outfile:
+            json.dump(secret_info, outfile, indent=4)
 
     @classmethod
-    def get_access_token(cls) -> str:
-        """json 파일로부터 액세스 토큰 정보 읽어 반환한다.
-
-        Args:
-           
-        Returns:
-            str : 액세스 토큰
-        """
-        with open(cls.file_path, "r") as json_file:
-            token_info = json.load(json_file)
-            return token_info["access_token"]
-
-    @classmethod
-    def get_refresh_token(cls):
-        """json 파일로부터 리프레시 토큰 정보 읽어 반환한다.
-
-        Args:
-           
-        Returns:
-            str : 리프레시 토큰
-        """
-        with open(cls.file_path, "r") as json_file:
-            token_info = json.load(json_file)
-            return token_info["refresh_token"]
-
-    @classmethod
-    def get_token_info(cls) -> Dict[str,str]:
+    def get_access_token_info(cls) -> Dict[str, str]:
         """액세스 토큰의 만료여부, 유효기간 등 정보를 확인한다. 
 
-        Args:
-           
         Returns:
-            Dict[str,str] : 반환 예시 참조
-            
+            엑세스 토큰 관련 정보를 담은 딕셔너리
+            example:
+
             {
                 'id': 2110957569, 
                 'expiresInMillis': 14132012, 
@@ -69,49 +59,41 @@ class Auth:
         access_token = cls.get_access_token()
         url = "https://kapi.kakao.com/v1/user/access_token_info"
         headers = {
-            "Authorization" : f"Bearer {access_token}"
+            "Authorization": f"Bearer {access_token}"
         }
-        response = requests.get(url=url,headers=headers)
+        response = requests.get(url=url, headers=headers)
         if response.status_code == 200:
             return response.json()
 
     @classmethod
-    def update__token(cls):
+    def update_token(cls) -> None:
         """액세스 토큰과 리프레시 토큰을 갱신한다.
-
-        Args:
-           
-        Returns:
-        
         """
-        refresh_token = cls.get_refresh_token()
-        print(refresh_token)
+        secret = cls.get_secret(['client_id','refresh_token'])
         url = "https://kauth.kakao.com/oauth/token"
         headers = {
-            "Content-Type" : "application/x-www-form-urlencoded"
+            "Content-Type": "application/x-www-form-urlencoded"
         }
         data = {
-            "grant_type" : "refresh_token",
-            "client_id" : f"{cls.client_id}",
-            "refresh_token" : f"{refresh_token}"
+            "grant_type": "refresh_token",
+            "client_id": f"{secret['client_id']}",
+            "refresh_token": f"{secret['refresh_token']}"
         }
-        response = requests.post(url=url,headers=headers,data=data)
+        response = requests.post(url=url, headers=headers, data=data)
         if response.status_code == 200:
             token_info = response.json()
             # 리프레시 토큰 값은 만료 기간이 1개월 미만으로 남았을 때 갱신되어 전달되기 때문에 응답에 리프레시 토큰이 있는지 확인한다.
             # https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api 참고
             if "refresh_token" in token_info:
-                cls.save_token(token_info["refresh_token"])
-            cls.save_token(token_info["access_token"])
+                cls.save_token(token_info['refresh_token'])
+            cls.save_token(token_info['access_token'])
         else:
             print(f"request failed with status: {response.status_code}")
 
     @classmethod
-    def get_tokens(cls) -> Dict[str,str]:
+    def get_tokens(cls) -> Dict[str, str]:
         """인가코드 를 통해 토큰 관련 정보를 반환하며 재실행 필요시 웹 브라우저를 통해 인가코드를 재발급 받아야 한다.
         https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-code 참고
-
-        Args:
 
         Returns:
             Dict[str,str] : 반환 예시 참조
@@ -126,16 +108,21 @@ class Auth:
             }
         """
         url = "https://kauth.kakao.com/oauth/token"
+        secret = cls.get_secret(["code"])
         headers = {
-            "Content-type" : "application/x-www-form-urlencoded;charset=utf-8"
+            "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
         }
         data = {
-            "grant_type" : "authorization_code",
-            "client_id" : "86289e71b93e7d9f67f4dcfbe69bc44d",
-            "redirect_uri" : "http://localhost:3000",
+            "grant_type": "authorization_code",
+            "client_id": "86289e71b93e7d9f67f4dcfbe69bc44d",
+            "redirect_uri": "http://localhost:3000",
             # 일회성 인가코드
-            "code" : "<code>"
+            "code": f"{secret['code']}"
         }
-        response = requests.post(url=url,headers=headers,data=data)
+        response = requests.post(url=url, headers=headers, data=data)
         return response.json()
 
+
+#  직접 실행시 토큰 정보 확인
+if __name__ == "__main__":
+    print(Auth.get_tokens())
